63f5938cd870e0704882fc6c8df02daa1e367a16
dave <dave@15d371ff-1b66-f64f-ac3d-4367063fff6b>
2019-04-18 20:34:05 +0000

    Bottle test server both returned headers AND static page (though template page could also be returned.

--- trunk/server/bottle_test_server.py
+++ trunk/server/bottle_test_server.py
@@ -1,44 +1,73 @@
-from bottle import route, run
-from requests import get
+# region - logging setup
+import logging
+logging.basicConfig()
+_LOG = logging.getLogger("bottle_test_server -- ")
+# _LOG.level = logging.INFO
+# _LOG.level = logging.TRACE
+# endregion
+
+import bottle as bt
+import requests as rq
 from threading import Thread
-from time import sleep
 import cfg
 
+# region - web page setup
 path = "/"
 srv = "http://%s:%s%s" % (cfg.SERVER_NAME, cfg.SERVER_PORT, path)
-pg = '''<!DOCTYPE HTML>
-<html>
-<head>
-<title>Title of the document</title>
-</head>
+static_path = 'static'
+static_page = "index.htm"
+previous_typed = "previously typed line"
+title = "new title"
+msg = "new message"
+assigned = "new assigned line"
+up_params = {'X-typed': previous_typed}
+down_params = {
+                'X-title': title,
+                'X-msg': msg,
+                'X-assigned': assigned,
+               }
+# endregion
 
-<body>
-The content of the document......
-</body>
 
-</html>
-'''
-
-params = {'typed':"typed line", 'assigned':"assigned"}
-
-
-@route(path)
+@bt.route(path)
 def hello():
-    return pg
 
+    _LOG.trace("Enter @get hello")
+#------------------------------------------------------------------------------
+    typed = bt.request.headers.get('X-typed')
+    for k, v in {**up_params, **down_params}.items():
+        bt.response.set_header(k, v)
+#     bt.response.body = "hello"
+#------------------------------------------------------------------------------
+    _LOG.trace("Leave @get hello")
+    with open("./static/index.htm") as f:
+        bt.response.body = f.read()
+    return bt.response
+
+
+pass
 #===============================================================================
-#
+# Web site above
+#-------------------------------------------------------------------------------
+# Test below
 #===============================================================================
 
 
 def tester():
-    r = get(srv)
-    assert r.text == pg
-    print("test complete")
+#     r = rq.get(srv)
+#     print(r)
+    r = rq.get(srv, headers=up_params)
+    print(r)
+    print(r.text)
+#     print(r.text)
+    print("\nHeaders --")
+    for h in r.headers:
+        print("    %s: %s" % (h, r.headers[h]))
+    print("\n*** test complete ***")
     return
 
 
 tthread = Thread(target=tester, name="test thread")
 tthread.start()
-run(host=cfg.SERVER_NAME, port=cfg.SERVER_PORT, debug=True)
+bt.run(host=cfg.SERVER_NAME, port=cfg.SERVER_PORT, debug=True)
 tthread.join()
