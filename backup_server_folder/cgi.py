a63eca5943c9504b592419dc0bfb08fe1c93fd61
Den <77911636+dncarac@users.noreply.github.com>
2020-03-03 04:10:13 +0000

    24: Server
    Task-Url: http://type-lines.com/mantis/view.php?id=24
    47: TaskData 
    
    Task-Url: http://type-lines.com/mantis/view.php?id=47
    : Search google 
    
    Task-Url:

---
+++ backup_server_folder/cgi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+src.server_cgi --
+
+Contains cgi code to run Flask
+
+Details from Flask documentation:
+    http://flask.pocoo.org/docs/0.12/deploying/cgi/
+
+
+__CreatedOn__ = "2018-05-31"
+__UpdatedOn__ = "2018-05-31"
+
+@author: den
+@copyright: Copyright 2018 den, ALL RIGHTS RESERVED
+'''
+
+pass
---
+++ tests/bare_server_test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+''' bare_server_test -
+
+@summary: Test remote operation of a testing test_server
+
+@description: Test module for test_server
+                receives a request.  
+                    If no headers: returns a static page
+                    If headers: returns a template with received headers and returned headers
+    
+__Created__="2019-06-20"
+__Updated__="2019-06-22"
+
+@version: 0.1
+@author: Den
+@copyright: Copyright © 2019
+@license: ALL RIGHTS RESERVED
+'''
+import bottle as bt; bt_app = bt.Bottle()
+if __name__ == '__main__':
+
+    pass
