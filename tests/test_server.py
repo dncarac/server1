ba43f6af453f3746148064ed8440bc458e1b133e
Den <77911636+dncarac@users.noreply.github.com>
2020-06-30 21:59:30 +0000

    Completed server, passed quick tests.  Need some more tests for error/exception cases.
    Commit server to test/implement cancel - i.e. terminate session.

--- tests/test_server.py
+++ tests/test_server.py
@@ -7,7 +7,7 @@ First test module
 Testing module for setting up Client-backup_server_folder communications
 
 __CreatedOn__="2018-04-02"
-__UpdatedOn__="2020-05-29"
+__UpdatedOn__="2020-06-30"
 
 @author: Den
 @copyright: Copyright © 2018-2020 Den
@@ -16,7 +16,7 @@ __UpdatedOn__="2020-05-29"
 import requests as rq    # @UnresolvedImport
 import cfg
 import pytest
-import sys
+# import sys
 # from TLexceptions import *
 
 # region - logging setup
@@ -26,52 +26,30 @@ import logging
 logging.basicConfig()
 _LOG = logging.getLogger(__name__)
 # _LOG.addFilter(trace_only)
-_LOG.level = logging.TRACE
+# _LOG.level = logging.TRACE
 # _LOG.level = logging.DEBUG
 # _LOG.level = logging.INFO
 # endregion
 
-url = cfg.SERVER
 
-
-@pytest.mark.skip("Successfully completed")
-def test_get():
-    ''' test_get --
-    Test get command.
-    Should produce HTML page.
-    '''
-    _LOG.trace("Enter test_get")
-#--------------------------------------------------------------------------------
-    r = rq.get(url)
-
-    _LOG.debug("r.text: %s" % r.text)
-    _LOG.debug("r.status_code: %s" % r.status_code)
-    assert r.text == cfg.DEFAULT_PAGE
-    assert r.status_code == cfg.SUCCESS
-#--------------------------------------------------------------------------------
-    _LOG.trace("Leave test_get")
-
-
-@pytest.mark.skip("Successfully completed")
+# @pytest.mark.skip("Successfully completed")
 def test_header_no_msg_type():
     ''' test_header_no_msg_type --
     '''
     _LOG.trace("Enter test_header_no_msg_type")
 #--------------------------------------------------------------------------------
-    r = rq.post(url)
+    r = rq.post(cfg.SERVER)
 
-    _LOG.debug("r.status_code: %s" % r.status_code)
+#     _LOG.debug("r.status_code: %s" % r.status_code)
     _LOG.debug("r.text: %s" % r.text)
     _LOG.debug("r.headers: %s" % r.headers)
-    _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers[cfg.RTN_KEY])
     assert r.status_code == cfg.CLIENT_ERROR
     assert r.text == ""
-    assert r.headers[cfg.RTN_KEY] == cfg.NACK
 #--------------------------------------------------------------------------------
     _LOG.trace("Leave test_header_no_msg_type")
 
 
-@pytest.mark.skip("Successfully completed")
+# @pytest.mark.skip("Successfully completed")
 def test_msg_type_bad_function():
     ''' test_msg_type_bad_function --
     '''
@@ -79,20 +57,18 @@ def test_msg_type_bad_function():
 #--------------------------------------------------------------------------------
     hdrs = {cfg.MSG_TYPE_KEY: "dummy"}
 
-    r = rq.post(url, data=hdrs)
+    r = rq.post(cfg.SERVER, data=hdrs)
 
     _LOG.debug("r.status_code: %s" % r.status_code)
     _LOG.debug("r.text: %s" % r.text)
     _LOG.debug("r.headers: %s" % r.headers)
-    _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers[cfg.RTN_KEY])
     assert r.status_code == cfg.NOT_IMPLEMENTED_ERROR
     assert r.text == ""
-    assert r.headers[cfg.RTN_KEY] == cfg.NACK
 #--------------------------------------------------------------------------------
     _LOG.trace("Leave test_msg_type_bad_function")
 
 
-@pytest.mark.skip("Successfully completed")
+# @pytest.mark.skip("Successfully completed")
 def test_SendFile_function_FOW():
     ''' test_SendFile_function_FOW --
     '''
@@ -102,20 +78,18 @@ def test_SendFile_function_FOW():
         files = {cfg.TASKFILE_KEY: open(cfg.TASK_PATH + "breast task 5.tsk", 'rb').read()}
         data = {cfg.MSG_TYPE_KEY: "SendFile"}
 
-        r = s.post(url, data=data, files=files)
+        r = s.post(cfg.SERVER, data=data, files=files)
 
         _LOG.debug("r.status_code: %s" % r.status_code)
         _LOG.debug("r.text: %s" % r.text)
         _LOG.debug("r.headers: %s" % r.headers)
-        _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers[cfg.RTN_KEY])
         assert r.status_code == cfg.SUCCESS
         assert r.text == ""
-        assert r.headers[cfg.RTN_KEY] == cfg.ACK
 #--------------------------------------------------------------------------------------------------
     _LOG.trace("Leave test_SendFile_function_FOW")
 
 
-@pytest.mark.skip("Successfully completed")
+# @pytest.mark.skip("Successfully completed")
 def test_SendFile_function_TL():
     ''' test_SendFile_function --
     '''
@@ -125,29 +99,26 @@ def test_SendFile_function_TL():
         files = {cfg.TASKFILE_KEY: open(cfg.TASK_PATH + "breast task 5.tlt", 'rb').read()}
         data = {cfg.MSG_TYPE_KEY: "SendFile"}
 
-        r = s.post(url, data=data, files=files)
+        r = s.post(cfg.SERVER, data=data, files=files)
 
         _LOG.debug("r.status_code: %s" % r.status_code)
         _LOG.debug("r.text: %s" % r.text)
         _LOG.debug("r.headers: %s" % r.headers)
-        _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers[cfg.RTN_KEY])
         assert r.status_code == cfg.SUCCESS
         assert r.text == ""
-        assert r.headers[cfg.RTN_KEY] == cfg.ACK
 #--------------------------------------------------------------------------------------------------
     _LOG.trace("Leave test_SendFile_function_TL")
 
 
-@pytest.mark.skip("Successfully completed")
+# @pytest.mark.skip("Successfully completed")
 def test_ClientConfig_no_session_data():
     ''' test_ClientConfig_no_session_data --
     '''
-    sys.stderr.write("\n\n")
     _LOG.trace("Enter test_ClientConfig_no_session_data")
 #-- Get client config-------------------------------------------------------------------------------------------
     data = {cfg.MSG_TYPE_KEY: "GetClientConfig"}
 
-    r = rq.post(url, data=data)
+    r = rq.post(cfg.SERVER, data=data)
 
     _LOG.debug("r.status_code: %s" % r.status_code)
     _LOG.debug("r.text: %s" % r.text)
@@ -155,7 +126,6 @@ def test_ClientConfig_no_session_data():
     _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers.get(cfg.RTN_KEY, None))
     assert r.status_code == cfg.NOT_FOUND_ERROR
     assert r.text == ""
-    assert r.headers[cfg.RTN_KEY] == cfg.NACK
 #--------------------------------------------------------------------------------------------------
     _LOG.trace("Leave test_ClientConfig_no_session_data\n\n")
 
@@ -164,86 +134,74 @@ def test_ClientConfig_no_session_data():
 def test_ClientConfig_no_client_config_data():
     ''' test_ClientConfig_no_client_config_data --
     '''
-    import TaskData as TD
-
-    sys.stderr.write("\n\n")
     _LOG.trace("Enter test_ClientConfig_no_client_config_data")
 #--------------------------------------------------------------------------------------------------
+    import TaskData as TD
     with rq.Session() as s:
     #-- Receive file------------------------------------------------------------------------------------------------
         fn = cfg.TASK_PATH + "no_config_data.tlt"
-        print("fn: %s" % fn)
         files = {cfg.TASKFILE_KEY: open(fn, 'rb').read()}
-        td = TD.TaskFile.read(cfg.TASK_PATH + 'no_config_data.tlt')
-        print(td)
+        td = TD.TaskFile().read(cfg.TASK_PATH + 'no_config_data.tlt')
         data = {cfg.MSG_TYPE_KEY: "SendFile"}
 
-        r = s.post(url, data=data, files=files)
+        r = s.post(cfg.SERVER, data=data, files=files)
 
         _LOG.debug("r.status_code: %s" % r.status_code)
         _LOG.debug("r.text: %s" % r.text)
         _LOG.debug("r.headers: %s" % r.headers)
-        _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers[cfg.RTN_KEY])
         assert r.status_code == cfg.SUCCESS
         assert r.text == ""
-        assert r.headers[cfg.RTN_KEY] == cfg.ACK
     #-- Get client config-------------------------------------------------------------------------------------------
         data = {cfg.MSG_TYPE_KEY: "GetClientConfig"}
 
-        r = s.post(url, data=data)
+        r = s.post(cfg.SERVER, data=data)
 
         _LOG.debug("r.status_code: %s" % r.status_code)
         _LOG.debug("r.text: %s" % r.text)
         _LOG.debug("r.headers: %s" % r.headers)
-        _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers.get(cfg.RTN_KEY, None))
         assert r.status_code == cfg.NOT_FOUND_ERROR
         assert r.text == ""
-        assert r.headers[cfg.RTN_KEY] == cfg.NACK
 #--------------------------------------------------------------------------------------------------
     _LOG.trace("Leave test_ClientConfig_no_client_config_data\n\n")
 
 
-@pytest.mark.skip("Successfully completed")
+# @pytest.mark.skip("Successfully completed")
 def test_ClientConfig_function():
-    ''' test_ClientConfig_function --
+    ''' Server.test_ClientConfig_function --
     '''
-    sys.stderr.write("\n\n")
-    _LOG.trace("Enter test_ClientConfig_function")
-#-- Send file -----------------------------------------------------------------------------------------------
+    _LOG.trace("Enter Server.test_ClientConfig_function")
+#  -------------------------------------------------------------------------------------------------
     with rq.Session() as s:
+#------ Send file -----------------------------------------------------------------------------------------------
         files = {cfg.TASKFILE_KEY: open(cfg.TASK_PATH + "breast task 5.tlt", 'rb').read()}
         data = {cfg.MSG_TYPE_KEY: "SendFile"}
 
-        r = s.post(url, data=data, files=files)
+        r = s.post(cfg.SERVER, data=data, files=files)
 
         _LOG.debug("r.status_code: %s" % r.status_code)
         _LOG.debug("r.text: %s" % r.text)
         _LOG.debug("r.headers: %s" % r.headers)
-        _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers[cfg.RTN_KEY])
         assert r.status_code == cfg.SUCCESS
         assert r.text == ""
-        assert r.headers[cfg.RTN_KEY] == cfg.ACK
+
 #------ Get client config-------------------------------------------------------------------------------------------
         data = {cfg.MSG_TYPE_KEY: "GetClientConfig"}
 
-        r = s.post(url, data=data)
+        r = s.post(cfg.SERVER, data=data)
 
         _LOG.debug("r.status_code: %s" % r.status_code)
         _LOG.debug("r.text: %s" % r.text)
         _LOG.debug("r.headers: %s" % r.headers)
-        _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers[cfg.RTN_KEY])
         assert r.status_code == cfg.SUCCESS
         assert r.text == ""
-        assert r.headers[cfg.RTN_KEY] == "{'hidden': False, 'rand_win_loc': False, 'lrg_button': True, 'rand_button_loc': False}"
 #--------------------------------------------------------------------------------------------------
-    _LOG.trace("Leave test_no_client_config_data\n\n")
+    _LOG.trace("Leave Server.test_ClientConfig_function\n\n")
 
 
-@pytest.mark.skip("Successfully completed")
+# @pytest.mark.skip("Successfully completed")
 def test_TypingData_function():
     ''' test_TypingData_functionv
     '''
-    sys.stderr.write("\n\n")
     _LOG.trace("Enter test_TypingData_function")
 #------ Send file -----------------------------------------------------------------------------------------------
     with rq.Session() as s:
@@ -253,17 +211,15 @@ def test_TypingData_function():
         data = {cfg.MSG_TYPE_KEY: "SendFile"}
         _LOG.debug("data sent: %s" % data)
 
-        r = s.post(url, data=data, files=files)
+        r = s.post(cfg.SERVER, data=data, files=files)
 
-        _LOG.debug("r.status_code (post): %s" % r.status_code)
-        _LOG.debug("r.text (post): %s" % r.text)
-        _LOG.debug("r.headers (post): %s" % r.headers)
-        _LOG.debug("r.headers[cfg.RTN_KEY] (post): %s" % r.headers[cfg.RTN_KEY])
+        _LOG.debug("r.status_code: %s" % r.status_code)
+        _LOG.debug("r.text: %s" % r.text)
+        _LOG.debug("r.headers: %s" % r.headers)
         assert r.status_code == cfg.SUCCESS
         assert r.text == ""
-        assert r.headers[cfg.RTN_KEY] == cfg.ACK
-#------ Send first line-------------------------------------------------------------------------------------------
-        _LOG.info("\n\nSend first line (cfg.START_TASK)")
+#------ Send lines----------------------------------------------------------------------------------
+        _LOG.info("\n\nSend lines")
         send_list = [cfg.START_TASK, "Line 1", "Line 2"]
         expected_list = ["{'title': 'Typing Dialog Title', 'line': 'Line 1', 'msg': 'Msg'}",
                          "{'title': 'Typing Dialog Title', 'line': 'Line 2', 'msg': 'Msg'}",
@@ -275,44 +231,77 @@ def test_TypingData_function():
                     cfg.TYPEDLINE_KEY: sent}
             _LOG.debug("data: %s" % data)
 
-            r = s.post(url, data=data)
+            r = s.post(cfg.SERVER, data=data)
 
             _LOG.debug("r.status_code: %s" % r.status_code)
             _LOG.debug("r.text: %s" % r.text)
             _LOG.debug("r.headers: %s" % r.headers)
-            _LOG.debug("r.headers[cfg.RTN_KEY]: %s" % r.headers[cfg.RTN_KEY])
             assert r.status_code == cfg.SUCCESS
             assert r.text == ""
             assert r.headers[cfg.RTN_KEY] == expected
-
 #--------------------------------------------------------------------------------------------------
     _LOG.trace("Leave test_TypingData_function\n\n")
-#
-#
-# def test_DistractionData_function():
-#     ''' test_DistractionData_function
-#     '''
-#     _LOG.trace("Enter test_DistractionData_function")
-# #--------------------------------------------------------------------------------------------------
-#     with rq.Session() as s:
-#         files = {FILE_KEY: open(cfg.TASK_PATH + "../tasks/test distraction data task.tsk", 'rb')}
-#
-#         data = {cfg.MSG_TYPE_KEY: "SendFile"}
-#         r = s.post(url, data=data, files=files)
-#         assert str(r) == "<Response [200]>"
-#         assert r.status_code == 200
-#         assert r.text == "200 Success - Task file received successfully"
-#         assert r.headers[cfg.RTN_KEY] == cfg.ACK
-#
-#         data = {cfg.MSG_TYPE_KEY: "GetDistractionData",
-#                 cfg.TYPEDLINE_KEY: None}
-#         r = s.post(url, data=data)
-#         assert str(r) == "<Response [200]>"
-#         assert r.status_code == 200
-#         assert r.text == "200 Success - Distraction data returned successfully"
-#         assert r.headers[cfg.RTN_KEY] == "{'title': 'Distraction Dialog Title', 'msg': 'Msg'}"
-# #--------------------------------------------------------------------------------------------------
-#     _LOG.trace("Leave test_DistractionData_function")
+
+
+# @pytest.mark.skip("Successfully completed")
+def test_DistractionData_function():
+    ''' test_DistractionData_function
+    '''
+    import sys
+    _LOG.trace("Enter test_DistractionData_function")
+#--------------------------------------------------------------------------------------------------
+    with rq.Session() as s:
+        files = {cfg.TASKFILE_KEY: open(cfg.TASK_PATH + "../tasks/test distraction data task.tsk", 'rb')}
+
+        data = {cfg.MSG_TYPE_KEY: "SendFile"}
+        r = s.post(cfg.SERVER, data=data, files=files)
+        assert r.status_code == cfg.SUCCESS
+        assert r.text == ""
+
+        data = {cfg.MSG_TYPE_KEY: "GetDistractionData",
+                cfg.TYPEDLINE_KEY: None}
+        r = s.post(cfg.SERVER, data=data)
+        assert r.status_code == 200
+        assert r.text == ""
+        assert r.headers[cfg.RTN_KEY] == "{'d_title': 'Distraction Title', 'd_msg': 'Message 1'}"
+
+        r = s.post(cfg.SERVER, data=data)
+        assert r.status_code == 200
+        assert r.text == ""
+        assert r.headers[cfg.RTN_KEY] == "{'d_title': 'Distraction Title', 'd_msg': 'Message 2'}"
+
+        r = s.post(cfg.SERVER, data=data)
+        assert r.status_code == 200
+        assert r.text == ""
+        assert r.headers[cfg.RTN_KEY] == "{'d_title': 'Distraction Title', 'd_msg': 'Message 1'}"
+#--------------------------------------------------------------------------------------------------
+    _LOG.trace("Leave test_DistractionData_function")
+
+
+@pytest.mark.skip("To be developed")
+def test_terminate_session_function():
+    ''' test_DistractionData_function
+    '''
+    _LOG.trace("Enter test_DistractionData_function")
+#--------------------------------------------------------------------------------------------------
+    with rq.Session() as s:
+        files = {cfg.TASKFILE_KEY: open(cfg.TASK_PATH + "../tasks/test distraction data task.tsk", 'rb')}
+
+        data = {cfg.MSG_TYPE_KEY: "SendFile"}
+        r = s.post(cfg.SERVER, data=data, files=files)
+        assert r.status_code == cfg.SUCCESS
+        assert r.text == ""
+
+        data = {cfg.MSG_TYPE_KEY: "Cancel"}
+        r = s.post(cfg.SERVER, data=data)
+        print("session s: %s" % s)
+        print("r.status_code: %s" % r.status_code)
+        print("r.text: %s" % r.text)
+        assert r.status_code == cfg.SUCCESS
+        assert r.text == ""
+
+#--------------------------------------------------------------------------------------------------
+    _LOG.trace("Leave test_DistractionData_function")
 
 
 if __name__ == "__main__":
@@ -329,6 +318,38 @@ if __name__ == "__main__":
 #     print(tf)
 #     td = TD.TaskFile().read(cfg.TASK_PATH + "no_config_data.tlt")
 #     print(td)
+#     exit()
+
+# Setup to create "breast task 5.tlt"
+#     import TaskData as TD
+#     tf = TD.TaskFile()
+#     print("tf: %s" % tf)
+#     td = tf.read(cfg.TASK_PATH + "breast task 5.tsk")
+#     print("td: %s" % td)
+#     print("tf: %s" % tf)
+#     tf.write(td, cfg.TASK_PATH + "breast task 5.tlt")
+#     print("tf: %s" % tf)
+#     exit()
+
+# Setup to create "test distraction data task.tsk"
+#     from TaskData import TaskData, TaskFile
+#     td = TaskData()
+#     print("td: %s" % td)
+#     print("td.distraction_data: %s" % td.distraction_data)
+#     print("td.distraction_data.d_titles: %s" % td.distraction_data.d_titles)
+#
+#     print("td.distraction_data.d_titles.lst: %s" % td.distraction_data.d_titles.lst)
+#     td.distraction_data.d_titles.lst = ['Distraction Title']
+#     print("td.distraction_data.d_titles.lst: %s" % td.distraction_data.d_titles.lst)
+#
+#     print("td.distraction_data.d_msgs.lst: %s" % td.distraction_data.d_msgs.lst)
+#     td.distraction_data.d_msgs.lst = ['Message 1', 'Message 2']
+#     print("td.distraction_data.d_msgs.lst: %s" % td.distraction_data.d_msgs.lst)
+#
+#     tf = TaskFile()
+#     print("tf: %s" % tf)
+#     tf.write(td, cfg.TASK_PATH + "test distraction data task.tsk")
+#     print("tf: %s" % tf)
 #     exit()
 
     ''' (from pytest docs):
@@ -352,3 +373,4 @@ if __name__ == "__main__":
 
     rtn = pytest.main(["-vvs", __file__])    # @UndefinedVariable
     print("Pytest returned:\n  %s - %s" % (rtn, exit_code[rtn]))
+
